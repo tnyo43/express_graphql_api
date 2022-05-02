f6ce330b2094c4a0a385b58dfc7e623c937532e1
tnyo43 <tnyo43@gmail.com>
2022-05-03 00:26:38 +0900

    export を修正

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,17 +1,19 @@
 import { GraphQLObjectType } from "graphql";
 import { userField } from "./user";
 
-export const queryType = new GraphQLObjectType({
+const query = new GraphQLObjectType({
   name: "Query",
   fields: {
     ...userField.query,
   },
 });
 
-export const mutationType = new GraphQLObjectType({
+const mutation = new GraphQLObjectType({
   name: "Mutation",
   description: "The root query type.",
   fields: {
     ...userField.mutation,
   },
 });
+
+export const fields = { query, mutation };
