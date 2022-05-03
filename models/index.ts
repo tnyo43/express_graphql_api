187120086fb7bfedc854e153d181485e05f7a2c4
tnyo43 <tnyo43@gmail.com>
2022-05-03 17:26:30 +0900

    models/index を typescript に対応させる

---
+++ models/index.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import UserFactory from "./user";
+
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+const db = {
+  sequelize,
+  Sequelize,
+  User: UserFactory(sequelize),
+};
+
+export default db;
