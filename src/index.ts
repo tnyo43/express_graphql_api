e8fe0476a8599c6bf2eb6fda2c4b0a6ab53fd325
tnyo43 <tnyo43@gmail.com>
2022-05-04 12:39:34 +0900

    cors に対応

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,22 @@ import * as express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { GraphQLSchema } from 'graphql';
 import { fields } from '~/fields';
+import * as cors from 'cors';
 
 const schema = new GraphQLSchema(fields);
 
 const app = express();
+
+const allowList = ['http://localhost:5000'];
+app.use(
+  cors((request, callback) => {
+    const origin = request.headers.origin;
+    const options = {
+      origin: origin && allowList.includes(origin)
+    };
+    callback(null, options);
+  })
+);
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
